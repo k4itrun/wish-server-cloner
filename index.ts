6e17159d5b179dd747b9e702ac55e6a1b13fd46e
k4itrun <103044629+k4itrun@users.noreply.github.com>
2025-04-08 16:42:30 -0500

    chmod for linux

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { Logger, chalk } from './utils';
-import { spawn } from 'node:child_process';
+import { spawn, spawnSync } from 'node:child_process';
 import path from 'node:path';
 import fs from 'node:fs';
 
@@ -11,22 +11,45 @@ console.log(chalk.bold('If you need help, contact me on Discord: ' + chalk.cyan.
 
 const EXE_NAME = 'wish-app';
 
-function getExecutablePath(): string {
+function getAppPath(): string {
   const platform = process.platform;
   const folder = platform === 'win32' ? 'windows' : 'linux';
   const extension = platform === 'win32' ? '.exe' : '';
   const exePath = path.resolve(process.cwd(), 'src', folder, EXE_NAME + extension);
 
   if (!fs.existsSync(exePath)) {
-    Logger('error', `Executable not found at path: ${exePath}`);
+    Logger('error', `App not found at path: ${exePath}`);
     process.exit(1);
   }
 
   return exePath;
 }
 
-function runExecutable(exePath: string): void {
+function ensureAppChmod(exePath: string): void {
+  try {
+    fs.chmodSync(exePath, 0o755);
+  } catch (err) {
+    Logger('warn', `fs.chmodSync failed, trying fallback with chmod command...`);
+
+    const result = spawnSync('chmod', ['+x', exePath]);
+    if (result.error || result.status !== 0) {
+      Logger('error', `Failed to set execution permissions using both methods.`);
+      Logger('info', `Use this manual command menten and try again: chmod +x ${exePath}`);
+      process.exit(1);
+    } else {
+      Logger('info', `Fallback chmod command succeeded.`);
+    }
+  }
+}
+
+
+function runApp(exePath: string): void {
   Logger('info', `Running ${EXE_NAME} on ${process.platform}...\n`);
+
+  if (process.platform === 'linux') {
+    ensureAppChmod(exePath);
+  }
+
   const child = spawn(exePath, [], { stdio: 'inherit' });
   attachEventHandlers(child);
 }
@@ -47,8 +70,8 @@ function attachEventHandlers(child: ReturnType<typeof spawn>): void {
 }
 
 function run(): void {
-  const exePath = getExecutablePath();
-  runExecutable(exePath);
+  const exePath = getAppPath();
+  runApp(exePath);
 }
 
 run();
